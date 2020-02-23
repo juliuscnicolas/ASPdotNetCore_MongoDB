ebf68068541bfe5a37760fb4b6eab81ee0e02155
jocn <juliuscnicolas@gmail.com>
2020-02-24 02:21:35 +0800

    initial commit

---
+++ AspNetCoreWebAppMongoDB/CarGalleryApp/Controllers/CarsController.cs
@@ -0,0 +1,113 @@
+﻿using CarGalleryApp.Models;
+using CarGalleryApp.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarGalleryApp.Controllers
+{
+    public class CarsController : Controller
+    {
+
+        private readonly CarService carService;
+
+        public CarsController(CarService carService)
+        {
+            this.carService = carService;
+        }
+
+        // GET: Cars
+        public ActionResult Index()
+        {
+            return View(carService.Get());
+        }
+
+        // GET: Cars/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Cars/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cars/Create
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create(IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        // GET: Cars/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Cars/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add update logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        // GET: Cars/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cars/Delete/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Delete(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                carService.Create(car);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(car);
+
+
+        }
+    }
+}
